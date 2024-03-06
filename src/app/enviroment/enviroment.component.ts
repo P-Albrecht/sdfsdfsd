137d29219f9248a9f7d088ecbdbc92f3b797ed2c
P-Albrecht <83654540+P-Albrecht@users.noreply.github.com>
2024-03-06 22:37:26 +0100

    Initial commit

---
+++ src/app/enviroment/enviroment.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enviroment',
+  templateUrl: './enviroment.component.html',
+  styleUrls: ['./enviroment.component.css']
+})
+export class EnviromentComponent {
+
+  constructor() {}
+
+  onClickMe() {
+    console.log('...........')
+  }
+}
---
+++ src/app/enviroment/enviroment.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { EnviromentComponent } from './enviroment.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    EnviromentComponent,
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    EnviromentComponent,
+  ],
+  providers: [
+  ],
+  bootstrap: [
+  ]
+})
+export class EnviromentModule { }
